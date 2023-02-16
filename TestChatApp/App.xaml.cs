79511eb33ac357cdee567c2c4e549747a017dacd
Jacob <jacob.hatwell@gmail.com>
2023-02-16 13:22:18 +0000

    Init from MAUI template

---
+++ TestChatApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestChatApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ TestChatApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestChatApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
